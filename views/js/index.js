84d02712dd2eca94d1b869c500631491ea4507bb
AudreyMeng <31713522+AudreyMeng@users.noreply.github.com>
2019-02-25 11:09:02 -0800

    mark places match query (#1)
    * mark places match query
    
    * resolve comments

--- views/js/index.js
+++ views/js/index.js
@@ -1,47 +1,106 @@
 var map; 
 var infoWindow;
 
+/***********************************************************************************************
+*Function Name : initMap
+*Description ：initialize google map to recenter on current position
+*Params ：null
+*Return ：null
+***********************************************************************************************/
 function initMap() {
-	var vancouver = new google.maps.LatLng(49.246292, -123.116226);
+  var vancouver = new google.maps.LatLng(49.246292, -123.116226);
 
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: vancouver,
-		zoom: 8
+    center: vancouver,
+    zoom: 8
 	}); // create map instance
-	infoWindow = new google.maps.InfoWindow();
+  infoWindow = new google.maps.InfoWindow();
 
 	if (navigator.geolocation) {
+		// Recenter map to current position.
 		navigator.geolocation.getCurrentPosition(function(position) {
             var pos = {
               lat: position.coords.latitude,
               lng: position.coords.longitude
-			};
-			
-			console.log('Your current position is:');
-			console.log(`Latitude : ${position.coords.latitude}`);
-			console.log(`Longitude: ${position.coords.longitude}`);
+            };
 
-			infoWindow.setPosition(pos);
-			infoWindow.setContent(position.name);
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
+      console.log('Your current position is:');
+      console.log(`Latitude : ${position.coords.latitude}`);
+      console.log(`Longitude: ${position.coords.longitude}`);
+
+			// This infoWindow operation is useless now since we have mark operation.
+      // infoWindow.setPosition(pos);
+      // infoWindow.setContent(position.name);
+      // infoWindow.open(map);
+      map.setCenter(pos);
+		}, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+		});
+
+		// TODO : listen to clicking or search event
+    markRequest("dmv california");
+
 	} else {
-		// Browser does not support Geolocation
-		handleLocationError(false, infoWindow, map.getCenter());
-	}	
+    // Browser does not support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-	infoWindow.setPosition(pos);
-	infoWindow.setContent(browserHasGeolocation ?
-						  'Error: The Geolocation service failed.' :
-						  'Error: Your browser doesn\'t support geolocation.');
-	infoWindow.open(map);
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+              'Error: The Geolocation service failed.' :
+              'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
 
+/***********************************************************************************************
+*Function Name : markRequest
+*Description ：
+	mark locations on map matching search query,
+	description shows up upon clicking marker.
+*Params ：place
+*Return ：null
+***********************************************************************************************/
+function markRequest(query) {
+  // Mark map with multiple locations matching query and
+  var request = {
+    query: query,
+    fields: ['name', 'geometry'],
+  };
+	
+  service = new google.maps.places.PlacesService(map);
+  // TODO : debug findPlaceFromQuery issue: only one/null result returned, 
+  // expected result should be a list of results.
+  service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+			console.log(`results.size()=${results.length}`);
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+		  } 
+		  // recenter map to first match result.
+		  // map.setCenter(results[0].geometry.location);
+    } 
+  });
+}
+
+/***********************************************************************************************
+*Function Name : createMarker
+*Description ：generate maker on map for specific place
+*Params ：place
+*Return ：null
+***********************************************************************************************/
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(place.name);
+    infoWindow.open(map, marker);
+  });
+}
 
 // Below needed to be refactored, not used currently, especially BMap should be removed
 (function(){
